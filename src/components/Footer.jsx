38a877b733763ca817f54553bf9305bd6be250a7
robby <robin.kw99@yahoo.com>
2023-03-16 23:21:51 +0530

    Minor changes made to the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,19 @@ import React from 'react';
 function Footer() {
 	let currentDate = new Date();
 	let year = currentDate.getFullYear();
-    
+
 	return (
 		<footer className="footer">
 			<p>
-				Designed by <a href="https://robin-khaoya.dev" target="_blank" rel="noopener noreferrer">Robin</a> &#169; {year} - All rights
-				reserved.
+				Designed by{' '}
+				<a
+					href="https://robin-khaoya.dev"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Robin
+				</a>{' '}
+				&#169; {year} - All rights reserved.
 			</p>
 		</footer>
 	);
